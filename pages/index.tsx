2de3d09f3892ed8d7ee2d09f42a8c9b60514e40c
hogreed <pushan45hsh@gmail.com>
2022-11-02 15:53:20 +0900

    #3 Tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return null;
-}
+import type { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-blue-500">it works</h1>
+    </div>
+  );
+};
+
+export default Index;
